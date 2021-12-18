380b71d182abbd42de47ab1bd4352a009673c409
el98 <elisefu@hotmail.com>
2021-12-18 22:49:28 +0100

    day 16 part 2

--- day16/day16.py
+++ day16/day16.py
@@ -25,57 +25,113 @@ def conv2bin(msg):
     return bin_msg
 
 
-def read_message(msg):
-    print(msg)
-    pack_vers = int(msg[0:3], 2)
-    pack_type = int(msg[3:6], 2)
+def operation(values, type_id):
+    if type_id == 0:
+        return sum(values)
+
+    elif type_id == 1:
+        mult = 1
+        for v in values:
+            mult *= v
+        return mult
+
+    elif type_id == 2:
+        return min(values)
+
+    elif type_id == 3:
+        return max(values)
+
+    elif type_id == 5:
+        if values[0] > values[1]:
+            return 1
+        return 0
+
+    elif type_id == 6:
+        if values[0] < values[1]:
+            return 1
+        return 0
+
+    else:
+        if values[0] == values[1]:
+            return 1
+        return 0
+
+
+def read_packet(msg):
+    version = int(msg[0:3], 2)
+    type_id = int(msg[3:6], 2)
 
     msg = msg[6:]
+
     # literal value
-    if pack_type == 4:
+    if type_id == 4:
         value = ''
         packet_size = 6
+
+        # read value group starting with 1
         while msg[0] == '1':
             value += msg[1:5]
             msg = msg[5:]
             packet_size += 5
+
+        # read last value group starting with 0
         value += msg[1:5]
-        print(int(value, 2))
         msg = msg[5:]
         packet_size += 5
-        return packet_size, pack_vers
 
+        return packet_size, version, int(value, 2)
+
+    # operator
     else:
         if msg[0] == '0':
-            subpack_length = int(msg[1:16], 2)
+            data_length = int(msg[1:16], 2)
             msg = msg[16:]
             read_size = 0
-            while read_size != subpack_length:
-                pack_length, subpack_vers = read_message(msg)
-                pack_vers += subpack_vers
-                msg = msg[pack_length:]
+            values = []
+
+            # read subpackets until the length of data read is reached
+            while read_size != data_length:
+                pack_length, subpack_vers, subvalue = read_packet(msg)
+                version += subpack_vers
+                values.append(subvalue)
                 read_size += pack_length
-            return subpack_length+7+15, pack_vers
+                msg = msg[pack_length:]
+
+            return read_size+7+15, version, operation(values, type_id)
 
         else:
             subpack_numb = int(msg[1:12], 2)
             msg = msg[12:]
-            subpack_length = 0
+            read_size = 0
+            values = []
+
+            # read subpackets until the number of packets read is reached
             for _ in range(subpack_numb):
-                pack_length, subpack_vers = read_message(msg)
-                subpack_length += pack_length
-                pack_vers += subpack_vers
+                pack_length, subpack_vers, subvalue = read_packet(msg)
+                version += subpack_vers
+                values.append(subvalue)
+                read_size += pack_length
                 msg = msg[pack_length:]
-            return subpack_length+7+11, pack_vers
+
+            return read_size+7+11, version, operation(values, type_id)
 
 
 def part_one():
 
     with open("input", "r") as f:
-        messages = [msg.strip() for msg in f.readlines()]
-        for msg in messages:
-            msg = conv2bin(msg)
-            print(read_message(msg))
+        msg = f.readline().strip()
+        msg = conv2bin(msg)
+
+        return read_packet(msg)[1]
 
 
-part_one()
+def part_two():
+
+    with open("input", "r") as f:
+        msg = f.readline().strip()
+        msg = conv2bin(msg)
+
+        return read_packet(msg)[2]
+
+
+print(part_two())
